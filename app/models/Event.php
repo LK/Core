b30203badb1a61c1609b2d59fd9258724036b071
Lenny Khazan <lenny.khazan@gmail.com>
2014-04-05 16:51:09 -0400

    Added event model

---
+++ app/models/Event.php
@@ -0,0 +1,32 @@
+<?php
+
+class Event extends \Eloquent {
+	protected $fillable = [];
+
+	protected $table = 'events';
+
+	public function getName()
+	{
+		return $this->name;
+	}
+
+	public function getBegins()
+	{
+		return $this->begins;
+	}
+
+	public function getEnds()
+	{
+		return $this->ends;
+	}
+
+	public function getOwner()
+	{
+		return $this->owner;
+	}
+
+	public function getDescription()
+	{
+		return $this->description;
+	}
+}
