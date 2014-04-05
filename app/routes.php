35468661068c110845d4f32992ba54c3d0d0027c
Joe Torraca <joe@joetorraca.com>
2014-04-05 08:56:19 -0400

    Removed trailing whitespace;

--- app/routes.php
+++ app/routes.php
@@ -7,3 +7,5 @@ Route::resource('session', 'SessionController');
 // Pretty login/logout routes
 Route::get('/login', 'SessionController@index');
 Route::get('/logoff', 'SessionController@destroy');
+
+Route::resource('register', 'RegisterController');
