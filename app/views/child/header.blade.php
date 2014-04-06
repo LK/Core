45d0ad4449218d1f94875fd1e941431669caf2c8
Joe Torraca <joe@joetorraca.com>
2014-04-05 21:21:26 -0400

    Made the nav bar look pretty

--- app/views/child/header.blade.php
+++ app/views/child/header.blade.php
@@ -1,3 +1,8 @@
 <nav>
-
+	<div class="left">
+		<a href="/" tabindex="1" class="white" id="nav-home" class="nav nelement logo" title="Home">TeamHack</a>
+	</div>
+	<div class="right">
+		<!--{{ HTML::linkRoute('register.index', 'Signup', null, array('class'=>'btn btn-success inline')) }}-->
+	</div>
 </nav>
--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -3,12 +3,16 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>@yield('title')</title>
+		<link href='http://fonts.googleapis.com/css?family=Raleway:400,200' rel='stylesheet' type='text/css'>
 		<link rel="stylesheet" href="/components/bootstrap/dist/css/bootstrap.min.css"/>
+		<link rel="stylesheet" href="/css/navigation.css"/>
+		<link rel="stylesheet" href="/css/style.css"/>
 		@yield('css')
 	</head>
 	<body>
-		@if ($navigation == '') @endif
+		@if ($navigation != '')
 		{{ $navigation }}
+		@endif
 
 		@yield('content')
 		
